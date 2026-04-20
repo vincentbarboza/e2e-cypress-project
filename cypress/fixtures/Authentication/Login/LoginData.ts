6fc890b3d107066d219d0f1cfa6f34a086d7f7a0
Vincent Barboza <vinnie.business@hotmail.com>
2026-04-20 07:38:44 +0200

    Test the pipeline rules

--- cypress/fixtures/Authentication/Login/LoginData.ts
+++ cypress/fixtures/Authentication/Login/LoginData.ts
@@ -2,7 +2,7 @@ import type {LoginFixture} from '../../../lib/Type/Authentication/LoginData';
 
 const loginFixture: LoginFixture = {
     userName: 'standard_user',
-    password: 'secret_sauce',
+    password: 'secret_sauce1',
 };
 
 export default loginFixture;
