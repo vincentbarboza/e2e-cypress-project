dbade1774c040e209e10a3d8d1ae7d65c73c3c52
Vincent Barboza <vinnie.business@hotmail.com>
2026-04-20 07:51:51 +0200

    Revert the changes of the test

--- cypress/fixtures/Authentication/Login/LoginData.ts
+++ cypress/fixtures/Authentication/Login/LoginData.ts
@@ -2,7 +2,7 @@ import type {LoginFixture} from '../../../lib/Type/Authentication/LoginData';
 
 const loginFixture: LoginFixture = {
     userName: 'standard_user',
-    password: 'secret_sauce1',
+    password: 'secret_sauce',
 };
 
 export default loginFixture;
